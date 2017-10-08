bf4cdf1ab727e197febffd9d86d55a9efec0ce85
fbormann <felipe.bormann@gmail.com>
2017-10-08 00:02:43 -0300

    feature(): add findviewbyid to fields on xml file

--- Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/ui/EpisodeDetailActivity.java
+++ Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/ui/EpisodeDetailActivity.java
@@ -2,6 +2,9 @@ package br.ufpe.cin.if710.podcast.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import br.ufpe.cin.if710.podcast.R;
 
@@ -12,6 +15,15 @@ public class EpisodeDetailActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_episode_detail);
 
-        //TODO preencher com informações do episódio clicado na lista...
+        TextView feedTitle = (TextView) findViewById(R.id.feed_item_title);
+        TextView feedDescription = (TextView) findViewById(R.id.feed_item_description);
+        TextView feedDownloadLink = (TextView) findViewById(R.id.feed_item_download_link);
+        TextView feedDate = (TextView) findViewById(R.id.feed_item_date);
+
+
+        feedTitle.setText(getIntent().getStringExtra("feed_title"));
+        feedDescription.setText(getIntent().getStringExtra("feed_description"));
+        feedDownloadLink.setText(getIntent().getStringExtra("feed_download_link"));
+        feedDate.setText(getIntent().getStringExtra("feed_date"));
     }
 }

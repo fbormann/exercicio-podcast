8c13c79df05e72a7291b1a0effed6411e2895b92
fbormann <felipe.bormann@gmail.com>
2017-10-13 20:54:08 -0300

    feature(): add update method on Provider

--- Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/db/PodcastProvider.java
+++ Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/db/PodcastProvider.java
@@ -68,7 +68,7 @@ public class PodcastProvider extends ContentProvider {
     @Override
     public int update(Uri uri, ContentValues values, String selection,
                       String[] selectionArgs) {
-        // TODO: Implement this to handle requests to update one or more rows.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return dbHelper.getWritableDatabase()
+                .update(PodcastDBHelper.DATABASE_TABLE, values, selection, selectionArgs);
     }
 }

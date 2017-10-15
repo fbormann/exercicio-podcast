1d12a32e4b04afc171cca764ca16d3b7a6178127
fbormann <felipe.bormann@gmail.com>
2017-10-15 14:36:32 -0300

    feature(): add stop and replay logic

--- Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/services/MediaPlayerService.java
+++ Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/services/MediaPlayerService.java
@@ -10,10 +10,14 @@ import android.os.IBinder;
 
 import java.io.File;
 
+import br.ufpe.cin.if710.podcast.resources.StatefulMediaPlayer;
+
 public class MediaPlayerService extends Service implements MediaPlayer.OnPreparedListener {
 
-    MediaPlayer player = null;
+    StatefulMediaPlayer player = null;
     private static final String ACTION_PLAY = "br.ufpe.cin.if701.podcast.action.PLAY";
+    private static final String ACTION_PAUSE = "br.ufpe.cin.if701.podcast.action.PAUSE";
+    File file = null;
 
     public MediaPlayerService() {
     }
@@ -26,20 +30,41 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnPrepare
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         if (intent.getAction().equals(ACTION_PLAY)) {
-            player = new MediaPlayer();
-            player.setAudioAttributes(new AudioAttributes.Builder()
-                    .setUsage(AudioAttributes.USAGE_MEDIA)
-                    .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH).build());
             String uri = intent.getStringExtra("file_uri");
             Uri myUri = Uri.parse(uri);
-            try {
-                player.setDataSource(this, myUri);
-                player.setOnPreparedListener(this);
-                player.prepareAsync();
-            } catch (Exception e) {
-                //none;
-            }
 
+            if (player != null) {
+                if (player.getDataSource().equals(myUri)) {
+                    player.seekTo(player.getCurrentPosition());
+                    player.start();
+                } else {
+                    try {
+                        player.setDataSource(this, myUri);
+                        player.setOnPreparedListener(this);
+                        player.prepareAsync();
+                    } catch (Exception e) {
+                        //none;
+                    }
+                }
+
+            } else {
+                player = new StatefulMediaPlayer();
+
+                player.setAudioAttributes(new AudioAttributes.Builder()
+                        .setUsage(AudioAttributes.USAGE_MEDIA)
+                        .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH).build());
+                try {
+                    player.setDataSource(this, myUri);
+                    player.setOnPreparedListener(this);
+                    player.prepareAsync();
+                } catch (Exception e) {
+                    //none;
+                }
+            }
+        }
+
+        if (intent.getAction().equals(ACTION_PAUSE)) {
+            player.pause();
         }
 
         return START_NOT_STICKY;
@@ -47,10 +72,22 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnPrepare
 
     public void onPrepared(MediaPlayer player) {
         player.start();
+        player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                mediaPlayer.release();
+                boolean deleted = file.delete();
+                System.out.println(deleted);
+            }
+        });
     }
+
+    public void setDataUri() {}
 
     @Override
     public void onDestroy() {
         if (player != null) player.release();
     }
+
+
 }
